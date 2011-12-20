21bac33f8d737c74a2dfca0451f6aea4194b021c
Helior Colorado <me@helior.info>
2011-12-20 12:23:56 -0800

    Dumping the code all in one huge commit.

--- README.txt
+++
@@ -1,2 +0,0 @@
-Master has no semantic meaning, shame on you.
-You might want to checkout a branch like 7.x-1.x
---
+++ contextual_forms.api.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Implements hook_contextual_forms_info().
+ * 
+ * Keys you may implement are:
+ * - title: The human-readable name
+ * - description: A short description of what the form does.
+ * - function: (optional) Specify the function name that is the form builder,
+ *   defaults to the machine name.
+ * - includes: (optional) Specify the file to include for the form builder, as
+ *   is expected by $form_state['build_info']['files'].
+ *   @see form_load_include(). Keys required are:
+ *     - module: (optional) The module owning the include file. Auto-derived.
+ *     - name: The name of file to load, without extension
+ *     - type: (optional) The type of include file, defaults to 'inc'
+ * - arguments: (optional) A keyed array of arguments to pass into the form
+ *   builder. These actually are converted to Ctools contexts. The keys must
+ *   represent existing Ctools contexts, and each item in the array should
+ *   provide the following keys:
+ *     - title: The label for the context
+ *     - type: the type of Ctools context class to use for generating the
+ *       context. Allowed values are "optional" or "required"
+ *     Alternatively, you can also just provide a string value for the label,
+ *     which will then use the default type of "required"
+ */
+function hook_contextual_forms_info() {
+  $forms['mymod_subscribe'] = array(
+    'title' => 'MyMod Subscription Form',
+    'description' => 'A form for requesting information from subscribers.',
+    'include' => array(      
+      // The form resides in mymod.admin.inc
+      'module' => 'mymod',
+      'name' => 'mymod.admin',
+      'type' => 'inc',
+    ),
+    'function' => 'mymod_subscribe',
+    'arguments' => array(
+      'node' => t('Node'),
+      'string' = array(
+        'title' => t('Operation'),
+        'type' => 'optional',
+      ),
+    ),
+  );
+  
+  return $forms;
+}
